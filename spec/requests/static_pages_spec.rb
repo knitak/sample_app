39d5e1eec815812dd81bb9e4c1be11fdb58f7a46
Ana Pedrosa <anacpedrosa@gmail.com>
2012-11-12 13:30:40 +0000

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,12 +2,21 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-	let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+	#let(:base_title) {"Ruby on Rails Tutorial Sample App"}
 
   subject { page }
 
+  #shared_examples_for "all static pages" do
+   # it { should have_selector('h1', text: heading) }
+    #it { should have_selector('h1', text: full_title(page_title)) }
+  #end
+
   describe "Home page" do
     before { visit root_path }
+    #let(:heading) {'Sample App'}
+    #let(:page_title) {''}
+
+    #it_should_behave_like "all static pages"
 
     #it "should have the h1 'Sample App'" do
     	# usa a função Capybara visit pr simular a visita a URI/static_pages/home num browser
@@ -18,8 +27,7 @@ describe "StaticPages" do
 
     #it "should have the base title" do
       #visit root_path
-    it { should have_selector 'title',
-                        :text => full_title('') }
+    it { should have_selector 'title',:text => full_title('') }
     
 
     #it "should not have a custom page title" do
@@ -35,6 +43,11 @@ describe "StaticPages" do
 
   describe "Help page" do
     before { visit help_path }
+    #let(:heading) {'Help'}
+    #let(:page_title) {'Help'}
+
+    #it_should_behave_like "all static pages"
+
   	#it "should have the h1 'Help'" do
   	#	visit root_path
   	it { should have_selector('h1', :text => 'Help') }
@@ -46,7 +59,11 @@ describe "StaticPages" do
   end 
 
   describe	 "About page" do
-     before { visit about_path }
+    before { visit about_path }
+    #let(:heading) {'About'}
+    #let(:page_title) {'About Us'}
+
+    #it_should_behave_like "all static pages"
   	#it "should have the h1 'About us'" do
   	#	visit root_path
   	it { should have_selector('h1',    text: 'About') }
@@ -57,8 +74,13 @@ describe "StaticPages" do
   
   end
 
-  describe	 "Contact page" do before { visit about_path }
-     before { visit contact_path }
+  describe	 "Contact page" do 
+    before { visit contact_path }
+
+    #let(:heading) {'Contact'}
+    #let(:page_title) {'Contact'}
+
+    #it_should_behave_like "all static pages"
   	#it "should have the h1 'Contact'" do
   	#	visit root_path
   	it { should have_selector('h1',    text: 'Contact') }
@@ -66,6 +88,38 @@ describe "StaticPages" do
     #  	it "should have the title 'Contact'" do
     #	visit root_path
     it { should have_selector('title', text: full_title('Contact')) }
-    
   end
+  
+  describe   "signup page" do 
+    before { visit signup_path }
+
+    #let(:heading) {'Sign up'}
+    #let(:page_title) {'Sign up'}
+
+    #it_should_behave_like "all static pages"
+    it { should have_selector('h1',    text: 'Sign up') }
+    it { should have_selector('title', text: full_title('Sign up')) }
+  end
+  #end
+
+  #it "should have the right links on the layout" do
+  #visit root_path
+
+  #click_link "About"
+  #page.should have_selector 'title', text: full_title('About Us')
+
+  #click_link "Help"
+  #page.should have_selector 'title', text: full_title('Help')
+
+  #click_link "Contact"
+  #page.should have_selector 'title', text: full_title('Contact')
+
+  #click_link "Home"
+  #click_link "Sign up now!"
+  #page.should have_selector 'title', text: full_title('Sign up')
+
+  #click_link "sample app"
+  #page.should have_selector 'title', text: full_title(' | Home') 
+  
+  #end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
-describe "UserPages" do
+describe "User pages" do
 subject {page}
 
   describe "signup page" do
   	before { visit signup_path}
-    it {should have_selector('h1', text:'Sign Up')}
+    it {should have_selector('h1', text:'Sign up')}
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-    it {should have_selector('title', text: full_title('Sign Up'))}
+    it {should have_selector('title', text: full_title('Sign up'))}
   end
 end
